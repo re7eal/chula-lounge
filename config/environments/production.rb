950f60bf9e77d1063f0bb780e4cd8dbc1a3443e5
Putti Orungrochkul <puttio@gmail.com>
2013-11-25 13:56:30 +0700

    fixed asset path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,9 +57,11 @@ ChulaLounge::Application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
 
+  config.assets.path << Rails.root.join('app', 'assets', 'fonts')
+
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( .svg .eot .woff .ttf )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
