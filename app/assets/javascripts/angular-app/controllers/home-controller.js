e39c3937b8dc958e350c473441dc96d0d1b0c544
Re7eaL <re7eal@re7eal-VirtualBox.(none)>
2013-11-25 20:01:53 +0700

    try delete angularjs wiki part

--- app/assets/javascripts/angular-app/controllers/home-controller.js
+++
@@ -1,8 +0,0 @@
-App.controller('PageIndex', ['$scope', '$http', function($scope, $http){
-
-	$http({method:'GET', url: '/faculties.json'}).
-		success(function(data){
-			$scope.faculties = data;
-			console.log($scope.faculties);		
-		});
-}]);
